fdd4aafc1fa8e156425706f7305a38ca51ad4d32
Will Woodward <will@willjw.co.uk>
2020-06-16 20:43:49 +0100

    Add home/docs controllers

---
+++ app/Http/Controllers/DocsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class DocsController extends Controller
+{
+    public function index(): View
+    {
+        return view('docs', [
+            'sections' => [
+                'competitions' => 'Competitions',
+                'seasons'      => 'Seasons',
+                'fixtures'     => 'Fixtures',
+                'results'      => 'Results',
+                'standings'    => 'Standings',
+                'top-scorers'  => 'Top Scorers',
+                'squads'       => 'Squads',
+            ],
+        ]);
+    }
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    public function index(): View
+    {
+        return view('home', [
+            'sections' => [
+                'competitions' => 'Competitions',
+                'seasons'      => 'Seasons',
+                'fixtures'     => 'Fixtures',
+                'results'      => 'Results',
+                'standings'    => 'Standings',
+                'top-scorers'  => 'Top Scorers',
+                'squads'       => 'Squads',
+            ],
+        ]);
+    }
+}
