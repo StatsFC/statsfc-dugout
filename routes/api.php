2068937c4de772d5f1a23d5425354b37bd9098ca
Will Woodward <will@willjw.co.uk>
2020-06-16 23:55:53 +0100

    Remove default /api prefix

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'auth', 'prefix' => '/api/v2'], function () {
+Route::group(['middleware' => 'auth', 'prefix' => 'v2'], function () {
     Route::get('/competitions', 'CompetitionsController@index');
 });
