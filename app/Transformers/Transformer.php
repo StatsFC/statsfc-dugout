24ce18e0dc5442c866a07b84f9a4eb2b1ecdb609
Will Woodward <will@willjw.co.uk>
2020-06-16 23:58:41 +0100

    Expect collection

--- app/Transformers/Transformer.php
+++ app/Transformers/Transformer.php
@@ -2,10 +2,12 @@
 
 namespace App\Transformers;
 
+use Illuminate\Database\Eloquent\Collection;
+
 abstract class Transformer
 {
-    public function transformCollection(array $items): array
+    public function transformCollection(Collection $items): array
     {
-        return array_map([$this, 'transform'], $items);
+        return array_map([$this, 'transform'], iterator_to_array($items));
     }
 }
