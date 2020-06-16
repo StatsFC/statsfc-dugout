907dae8f2fbbf008fd1a7b08e758d526f5af30eb
Will Woodward <will@willjw.co.uk>
2020-06-16 23:47:32 +0100

    Use web routes

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::group(['middleware' => 'auth', 'prefix' => '/v2'], function () {
-    Route::get('/competitions', 'CompetitionsController@index');
-});
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 Route::get('/docs', 'DocsController@index');
+
+Route::group(['middleware' => 'auth', 'prefix' => '/api/v2'], function () {
+    Route::get('/competitions', 'CompetitionsController@index');
+});
