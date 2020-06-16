6b0df8c14e0d51385e332185f6f62f9ab94b1254
Will Woodward <will@willjw.co.uk>
2020-06-16 21:10:44 +0100

    Add first API route with auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,61 @@
 
 namespace App\Http\Middleware;
 
+//use App\Customer;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    const API_TEST_KEY = 'apitest';
+
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (App::isDownForMaintenance()) {
+            return $this->respondWithError($request, $next, 503, 'Down for maintenance. We\'ll be right back');
         }
+
+        $key = $request->header('X-StatsFC-Key');
+
+        if (! $key) {
+            return $this->respondWithError($request, $next, 401, 'API key not provided');
+        }
+
+        /*$customers = Customer::where('key', $key)->get();
+
+        if ($customers->count() !== 1) {
+            return $this->respondUnauthorised('API key not found');
+        }
+
+        $customer = $customers->first();
+
+        if ($key !== static::API_TEST_KEY && ! $this->authenticateRequestIp($request, $customer)) {
+            return $this->respondUnauthorised('IP address does not match');
+        }
+
+        if ($this->hasRequestedInvalidCompetition($request, $customer)) {
+            return $this->respondUnauthorised('The chosen competition is not in your API subscription');
+        }
+
+        if ($this->hasExceededRateLimit($customer)) {
+            return $this->respondTooManyRequests();
+        }
+
+        // Put the customer into a session
+        $request->session()->put('customer_id', $customer->id);*/
+
+        return $next($request);
+    }
+
+    protected function respondWithError(Request $request, Closure $next, int $status, string $message)
+    {
+        return $next($request)->respond([
+            'error' => [
+                'message'    => $message,
+                'statusCode' => $status,
+            ],
+        ]);
     }
 }
