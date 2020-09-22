c33bc0139058c7be154ed0e95784cff616a3a93d
Will Woodward <will@willjw.co.uk>
2020-09-22 15:57:18 +0100

    Move docs nav to the left/top

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -6,16 +6,6 @@
     <p class="text-center home-tagline">The complete documentation of our JSON API</p>
 
     <div class="row">
-        <div class="col-md-9">
-            @foreach ($sections as $id => $name)
-                <article id="{{ $id }}">
-                    <h3>{{ $name }}</h3>
-
-                    @include('docs.' . $id)
-                </article>
-            @endforeach
-        </div>
-
         <div class="col-md-3">
             <div id="docs-nav">
                 <div class="list-group">
@@ -25,42 +15,54 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-md-9">
+            @foreach ($sections as $id => $name)
+                <article id="{{ $id }}">
+                    <h3>{{ $name }}</h3>
+
+                    @include('docs.' . $id)
+                </article>
+            @endforeach
+        </div>
     </div>
 @stop
 
 @section('script')
     <script>
-        $(function() {
-            var $nav        = $('#docs-nav');
-            var offsetTop   = $nav.offset().top;
-            var startSticky = (offsetTop - 20);
+        $(function () {
+            let $nav = $('#docs-nav');
+            let offsetTop = $nav.offset().top;
+            let startSticky = (offsetTop - 20);
 
-            $(document).on('scroll', function() {
-                var top = $(document).scrollTop();
+            $(document).on('scroll', function () {
+                let top = $(document).scrollTop();
 
                 if (top > startSticky) {
                     $nav.css({
                         position: 'fixed',
-                        top:      '20px',
-                        width:    $nav.parent().width()
+                        top: '20px',
+                        width: $nav.parent().width(),
                     });
                 } else {
                     $nav.css({
-                        position: 'relative'
+                        position: 'relative',
                     });
                 }
             });
 
-            $('.list-group-item').on('click', function() {
+            $('.list-group-item').on('click', function () {
                 $('.list-group-item.active').removeClass('active');
                 $(this).addClass('active');
             });
 
-            $('#accordion .panel-heading a').append(
-                $('<i>').addClass('fa fa-arrow-down pull-right')
+            let $accordionLinks = $('#accordion .panel-heading a');
+
+            $accordionLinks.append(
+                $('<i>').addClass('fa fa-arrow-down pull-right'),
             );
 
-            $('#accordion .panel-heading a').on('click', function(e) {
+            $accordionLinks.on('click', function () {
                 $(this).find('i').toggleClass('fa-arrow-down').toggleClass('fa-arrow-up');
             });
         });
